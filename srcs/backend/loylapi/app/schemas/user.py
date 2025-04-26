fd9d2689f6e52414aa4e626be19e9b4a3f5c555a
edvin3i <edviniii@gmail.com>
2025-04-26 15:14:25 +0200

    add schema for user

---
+++ srcs/backend/loylapi/app/schemas/user.py
@@ -0,0 +1,18 @@
+from app.schemas.base import BaseSchema, BaseDBSchema
+from pydantic import EmailStr
+from app.schemas.types import PhoneStr, PrivyIDStr
+
+
+class UserCreate(BaseSchema):
+    privy_id: PrivyIDStr
+    phone: PhoneStr
+    email: EmailStr
+
+
+class UserUpdate(BaseSchema):
+    phone: PhoneStr | None = None
+    email: EmailStr | None = None
+
+
+class UserOut(UserCreate, BaseDBSchema):
+    pass
