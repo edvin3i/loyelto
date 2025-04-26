29c94c0fc5529c31684f56aba924cd120efb5d25
edvin3i <edviniii@gmail.com>
2025-04-26 23:10:59 +0200

    add schemas for transactions and voucher

---
+++ srcs/backend/loylapi/app/schemas/voucher.py
@@ -0,0 +1,51 @@
+import datetime
+from uuid import UUID
+from pydantic import AnyUrl
+from app.schemas.base import BaseSchema, BaseDBSchema
+from app.schemas.types import (
+    NameStr,
+    TextAreaStr,
+    Decimals,
+    GeZero,
+    MintStr,
+    MediumStr,
+    VouchStatEnum,
+)
+
+
+class VoucherTemplateCreate(BaseSchema):
+    business_id: UUID
+    name: NameStr
+    description: TextAreaStr | None
+    image_url: AnyUrl | None
+    price_points: Decimals
+    supply: GeZero
+    expires_at: datetime | None
+    collection_mint: MintStr | None
+
+
+class VoucherTemplateOut(BaseDBSchema):
+    business_id: UUID
+    name: NameStr
+    description: TextAreaStr | None
+    image_url: AnyUrl | None
+    price_points: Decimals
+    supply: GeZero
+    expires_at: datetime | None
+    collection_mint: MintStr | None
+
+
+class VoucherNFTCreate(BaseSchema):
+    template_id: UUID
+    user_id: UUID | None
+    asset_id: MediumStr
+    status: VouchStatEnum
+    redeemed_at: datetime | None
+
+
+class VoucherNFTOut(BaseDBSchema):
+    template_id: UUID
+    user_id: UUID | None
+    asset_id: MediumStr
+    status: VouchStatEnum
+    redeemed_at: datetime | None
