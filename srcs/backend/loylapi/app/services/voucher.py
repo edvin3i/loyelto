de2ace1a7704c3c90e8455898444005f2d8dee97
edvin3i <edviniii@gmail.com>
2025-05-13 22:44:55 +0200

    add vouchers of user

---
+++ srcs/backend/loylapi/app/services/voucher.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from uuid import UUID
+from typing import List
+from app.models import VoucherNFT
+
+class VoucherService:
+    async def user_vouchers(self, db: AsyncSession, user_id: UUID) -> List[VoucherNFT]:
+        stmt = select(VoucherNFT).where(VoucherNFT.user_id == user_id)
+        res = await db.execute(stmt)
+        return list(res.scalars())
+
+voucher_service = VoucherService()
