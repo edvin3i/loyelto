45dac713dfdbf408da7f12b7a934221bf45389ed
Svetlana Vydrina <nyagalen@gmail.com>
2025-05-20 18:35:32 +0200

    made the coin image a lil less koryavy

--- srcs/frontend/webapp/loyelto-web/src/business-app/BusinessMain.tsx
+++ srcs/frontend/webapp/loyelto-web/src/business-app/BusinessMain.tsx
@@ -14,9 +14,11 @@ export default function BusinessMain() {
                 <Typography variant='h4'>My balance</Typography>
                 <Stack direction="row" spacing={1}>
                     <Typography variant='h1'>56679</Typography>
+                    <Box sx={{paddingTop: 2}}>
                     <Image src="coin_loyl.png"
-                        sx={{ height: '5.5rem', width: '5.5rem' }}
+                        sx={{ height: '5.3rem', width: '5.3rem' }}
                     />
+                    </Box>
                 </Stack>
                 <Typography variant='subtitle2'>points that you've distributed to the consumers</Typography>
 
