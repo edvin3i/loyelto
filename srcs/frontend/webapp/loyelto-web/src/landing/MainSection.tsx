0a084ea5e0dd19585794b02a2b2207f45043fb75
Svetlana Vydrina <nyagalen@gmail.com>
2025-05-27 18:56:21 +0200

    desktop version looking almost good

--- srcs/frontend/webapp/loyelto-web/src/landing/MainSection.tsx
+++ srcs/frontend/webapp/loyelto-web/src/landing/MainSection.tsx
@@ -14,76 +14,73 @@ export default function MainSection() {
     const { t } = useTranslation();
     return (
 
-        <Stack direction={{ xs: 'column-reverse', sm: 'row' }} sx={{marginBottom: 5}}>
+        <Stack direction={{ xs: 'column-reverse', sm: 'row' }} sx={{ marginBottom: 5 }}>
             <Box sx={{
-                // backgroundColor: theme.palette.secondary.dark,
                 width: { sm: '50%', xs: '100%' },
                 display: 'flex',
                 flexDirection: 'column',
                 justifyContent: "center",
-                
-                // padding: 2
             }}>
                 <Typography
                     variant="h1"
                     color="initial"
                     gutterBottom
                     sx={{
-                        fontSize: { xs: '2.5rem', sm: '4.1rem', xl: '8rem' },
-                        // fontSize: 'clamp(4vw, 8rem, 10vw)',
+                        fontSize: { xs: '2.5rem', sm: '4.1rem', xl: '5.5rem' },
                         fontWeight: '600',
-                        textAlign: {xs:'center', sm:'left'},
-                        // marginY: 3
-                        marginBottom: {xs: 2, sm: 9},
+                        textAlign: { xs: 'center', sm: 'left' },
+                        marginBottom: { xs: 2, sm: 4 },
                         marginTop: 2
-                    }}
-                >
+                    }}>
                     Swap & Save: <br /> {t('mainHeading')}
                 </Typography>
-                <Box sx={{ marginLeft: {sm: 0}, marginTop: 1, marginX: {xs: 2}}}>
-                    <Button 
-                        fullWidth 
-                        size="large" 
+                <Box sx={{ marginLeft: { sm: 0 }, marginTop: 1, marginX: { xs: 2 } }}>
+                    <Button
+                        fullWidth
+                        size="large"
                         variant="contained"
                         onClick={() => {
-                            document.getElementById('join-waitlist')?.scrollIntoView({ 
-                                behavior: 'smooth' 
+                            document.getElementById('join-waitlist')?.scrollIntoView({
+                                behavior: 'smooth'
                             });
                         }}
                         sx={{
                             alignSelf: 'center',
-                            // margin: 3,
                             fontWeight: 'bold',
-                            paddingY: {sm:1.5, xs: 1},
+                            paddingY: { sm: 1.5, xs: 1 },
                             borderRadius: 3,
                             textTransform: 'none',
-                            fontSize: {sm:'1.5rem', xs: '1.2rem'}, 
-                           
-                        }}
-                    >
+                            fontSize: { sm: '1.5rem', xs: '1.2rem' },
+                        }}>
                         {t('joinLoyeltoButton')}
                     </Button>
                 </Box>
             </Box>
             <Box
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
+                sx={{  
                     width: { sm: '50%', xs: '100%' },
-                    height: { xs: '96vw', sm: 'auto' },
+                    height: { xs: '96vw', sm: '60vh' },
                     backgroundColor: theme.palette.secondary.main,
-                    borderRadius: 7,
-                    overflow: 'hidden' // Ensures the image is cropped if it overflows
+                    borderRadius: 7
                 }}>
-                <Image src='ecran_julien_nobg.png'
-                    sx={{
-                        width: { xs: '85%', sm: '50%' },
-                        marginTop: { xs: 4, sm: 7 },
-                        marginBottom: { xs: 'auto', sm: 7 },
+                <Stack direction="row" sx={{height: '100%'}}>
+                    <Box>
+                        <Image src='landing_swapping_cutout.png'
+                            sx={{
+                                width: { xs: '85%' },
+                                marginTop: { xs: 4, sm: 10 },
+                            }} />
+                    </Box>
+                    <Box sx={{ overflow: 'hidden'}}>
+                        <Image src='ecran_julien_nobg.png'
+                            sx={{
+                                width: { xs: '85%', sm: '95%' },
+                                marginTop: { xs: 4, sm: 9 },
+                                objectFit: 'cover', // Ensures the image is cropped to fit the parent
+                            }} />
+                    </Box>
+                </Stack>
 
-                        objectFit: 'cover', // Ensures the image is cropped to fit the parent
-                    }} />
             </Box>
         </Stack>
     )
