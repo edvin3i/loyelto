56635d14414c02d92097dd6e23d00ffb8416e96a
Ivan <42934110+edvin3i@users.noreply.github.com>
2025-06-15 19:57:07 +0200

    Add LoylToken client and update transfer logic

--- srcs/backend/loylapi/app/tasks/transfer.py
+++ srcs/backend/loylapi/app/tasks/transfer.py
@@ -11,13 +11,13 @@ from app.services.celery_wrapper import log_task
 @celery.task(name="onchain.transfer_earn", queue="onchain", bind=True, max_retries=3)
 @log_task
 def transfer_earn_task(
-    self, *, business_kp_b58: str, mint: str, user_pubkey: str, amount: int
+    self, *, mint: str, user_pubkey: str, amount: int
 ):
     """
     Celery wrapper for business → user transfer.
     """
     try:
-        earn_token(mint, user_pubkey, business_kp_b58, amount)
+        earn_token(mint, user_pubkey, amount)
     except Exception as exc:
         raise self.retry(exc=exc, countdown=10)
 
@@ -25,13 +25,13 @@ def transfer_earn_task(
 @celery.task(name="onchain.transfer_redeem", queue="onchain", bind=True, max_retries=3)
 @log_task
 def transfer_redeem_task(
-    self, *, user_pubkey: str, mint: str, business_pubkey: str, amount: int
+    self, *, user_pubkey: str, mint: str, amount: int
 ):
     """
     Celery wrapper for user → business transfer.
     """
     try:
-        redeem_token(mint, user_pubkey, business_pubkey, amount)
+        redeem_token(mint, user_pubkey, amount)
     except Exception as exc:
         raise self.retry(exc=exc, countdown=10)
 
